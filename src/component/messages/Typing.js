5b6618c6c238cd9af7d45c98c8d412aa7333bd0e
glen <glen_sale@yahoo.com>
2019-03-10 15:35:11 -0700

    it will create a typing style and user details

---
+++ src/component/messages/Typing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Typing = () => {
+    return (
+        <div className="typing">
+            <div className="typing__dot" />
+            <div className="typing__dot" />
+            <div className="typing__dot" />
+        </div>
+    );
+};
---
+++ src/component/messages/User.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const User = () => {
+    return (
+        <div className="user">
+            <div className="user__avatar" />
+            <div className="user__author" />
+            <div className="user__details" />
+        </div>
+    );
+};
