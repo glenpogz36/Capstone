0b8478a6414d1ce858dec63040c1bad9e46c4fb9
glen <glen_sale@yahoo.com>
2019-03-10 14:39:16 -0700

    it will add new route and move rgister state inside the App.js

--- src/component/App.js
+++ src/component/App.js
@@ -1,30 +1,60 @@
 
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom'
 import Navbar from './layout/Navbar'
 import Dashboard from './dashboard/Dashboard'
 import ProjectDetails from './project/ProjectDetails'
 import SignIn from './auth/SignIn'
 import SignUp from './auth/SignUp'
 import CreateProject from './project/CreatePoject'
+import { connect } from "react-redux";
+import { setUser, clearUser } from "../actions";
+import "../css/app.css";
+import Layout from "../components/Layout";
+import { Spinner } from "../helpers/spinner";
 
 class App extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push("/message");
+      } else {
+        this.props.history.push("/signin");
+        this.props.clearUser();
+      }
+    });
+  }
+
   render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Dashboard} />
-            <Route path='/project/:id' component={ProjectDetails} />
-            <Route path='/signin' component={SignIn} />
-            <Route path='/signup' component={SignUp} />
-            <Route path='/createProject' component={CreateProject} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={Dashboard} />
+              <Route exact path="/message" component={Layout} />
+              <Route path='/project/:id' component={ProjectDetails} />
+              <Route path='/signin' component={SignIn} />
+              <Route path='/signup' component={SignUp} />
+              <Route path='/createProject' component={CreateProject} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+
+      );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading
+
+});
+
+export default connect(
+  mapStateToProps,
+  { setUser, clearUser }
+)(withRouter(App));
+
